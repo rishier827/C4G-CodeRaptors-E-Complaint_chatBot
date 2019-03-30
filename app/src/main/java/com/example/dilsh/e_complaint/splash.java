be8021e232aa0434f73f5d241acd78739d725134
Dilshan De Almeida <DilshanDeAlmeida2017802@users.noreply.github.com>
2019-03-30 16:55:22 +0530

    add1

--- app/src/main/java/com/example/dilsh/e_complaint/splash.java
+++ app/src/main/java/com/example/dilsh/e_complaint/splash.java
@@ -3,11 +3,11 @@ package com.example.dilsh.e_complaint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SubmitComplaint extends AppCompatActivity {
+public class splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_submit_complaint);
+        setContentView(R.layout.activity_splash);
     }
 }
